0d0dc9a85c4634eb924c7f1c3ff020ed20a697b4
ADJDK <adrikdan@gmail.com>
2023-05-08 13:24:57 +0000

    Archive: Complete python-trying

---
+++ python-trying/main.py
@@ -0,0 +1,13 @@
+import time
+print("Hello World!!")
+time.sleep(1)
+print("Hello World!!")
+name=input("What is your name?")
+print("Hello",name)
+score=5
+print(score)
+cha=len("adrik")
+print(cha)
+for i in range(10):
+    print(i)
+    time.sleep(1)
