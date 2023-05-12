f883e8668586295499329ad935644448718119f1
ADJDK <adrikdan@gmail.com>
2023-05-12 10:11:56 +0000

    Archive: Complete python-type-trying

---
+++ python-type-trying/main.py
@@ -0,0 +1,13 @@
+'''import time
+while True:
+    print("hello world")
+    time.sleep(1)'''
+#IMPORT RANDOM
+import random
+print(random.randint(1,6))
+num=int(input("first no"))
+num1=int(input("second no"))
+print(num+num1)
+def adrik_jose():
+    print("Hello from a function!")
+adrik_jose()
