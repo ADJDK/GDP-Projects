3157e8400bab1a03ef2c6ff63d5b6dde479dae75
ADJDK <adrikdan@gmail.com>
2023-05-13 06:25:13 +0000

    Archive: Complete chatbot

---
+++ chatbot/main.py
@@ -0,0 +1,14 @@
+name=input("what is your name?")
+print('hi',name,'welcome to my chatbot')
+age=int(input("what is your age?"))
+if age>10:
+    print('Damn! Your old')
+else:
+    print("You are so yound")
+hobby=input("do you have a hobby?")
+a='yes'
+if hobby==a:
+    hob=input("what is your hobby?")
+else:
+    print('ohhh, anyway moving on')
+print("wow!",hob,',that is a nice hobby')
