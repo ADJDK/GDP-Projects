26a1712fafa265b8e57c20a9be1fc26fe75c94ac
ADJDK <adrikdan@gmail.com>
2023-05-14 12:57:10 +0000

    Archive: Complete my-name-fav-hobby-and-holiday-destination

---
+++ my-name-fav-hobby-and-holiday-destination/main.py
@@ -0,0 +1,8 @@
+print('my name is adrik')
+print('my fav hobby is playing games')
+print('my fav holiday destination is india')
+print('it is the best',end=' ')
+print('place in the world') 
+print('it has a lot of cool places','views to see',sep=' & ')
+
+
