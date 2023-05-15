a02d5b2e55f5b4d00f62f57ff54385c6de3687aa
ADJDK <adrikdan@gmail.com>
2023-05-15 15:53:44 +0000

    Archive: Complete escape-and-type-functions

---
+++ escape-and-type-functions/main.py
@@ -0,0 +1,14 @@
+print("my name is \'adrik\'")
+print("i like games like:\ngta\ncod\npubg")
+print("\\the best")
+print('my fav car is\tnissan')
+print('i like\r tosee')
+x=5
+print(type(x))
+y=2.4
+print(type(y))
+z=[1,2,3,4]
+print(type(z))
+w=(1,2,3,4)
+print(type(w))
+print("Adam is a \"good\" boy.\nHe likes playing \'games\'")
