c542210b50e08d9dc73d9260136cf119129e7658
ADJDK <adrikdan@gmail.com>
2023-05-15 13:23:14 +0000

    Archive: Complete python-terms

---
+++ python-terms/main.py
@@ -0,0 +1,27 @@
+myfile='adrik'
+Myfile='damn'
+_data_123='cars'
+d_a_t_a='fish'
+print(myfile)
+print(Myfile)
+print(_data_123)
+print(d_a_t_a)
+
+x=10
+print(x)
+print(type(x))
+y=3.6
+print(y)
+print(type(y))
+z=[1,2,3]
+print(z)
+print(type(z))
+
+a=int(input('enter the no:'))
+b=int(input('enter the second no:'))
+c=a+b
+print(c)
+string='my name is adrik'
+char='a'
+print(string)
+print(char)
