e7adbffd5a54139bca73daefea7c1e7a14d376e3
ADJDK <adrikdan@gmail.com>
2023-05-18 13:21:46 +0000

    Archive: Complete format

---
+++ format/main.py
@@ -0,0 +1,25 @@
+'''sage='hi,Welcome to brightchamps'
+print(message)
+name=input('what is your name:')
+print('hi,',name,'welcome to brightchamps')
+num=int(input('enter the no'))
+num1=int(input('enter the second no'))
+print(num+num1)
+print('Welcome to python class')
+print('please enter your name:')
+name=input()
+print('please enter your age:')
+age=int(input())
+print('Hi {},your age is {}'.format(name,age))
+print('Hi {},your age is '.format(name,age))
+#errorprint('Hi {},your age is {}'.format(name))#error
+print('Hi {},your age is {},my name is {}, age {}'.format(name,age,name,age))
+print('Hi {},your age is {}'.format(age,name))
+print('Hi {1},your age is {0}'.format(age,name))
+print('Hi {0},your age is {1}'.format(name,age))'''
+a=13
+b=300
+if b>a:
+    print('b is greater than a')
+
+    
