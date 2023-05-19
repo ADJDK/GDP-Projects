c41f50ef8b414b49f58487d64ddfb47a9bc5be88
ADJDK <adrikdan@gmail.com>
2023-05-19 05:05:25 +0000

    Archive: Complete scam

---
+++ scam/main.py
@@ -0,0 +1,6 @@
+print('Enter your name')
+name=input()
+print(' '*20,'congrats',' '*20)
+print('\n')
+print("{}, You are our 1 millionth customer,\n to get your award pls enter your contact details ".format(name))
+
