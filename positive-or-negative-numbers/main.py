c3fd6e20e4ff27c25ca1a7e9a1a124f23f954cec
ADJDK <adrikdan@gmail.com>
2023-05-25 12:09:12 +0000

    Archive: Complete positive-or-negative-numbers

---
+++ positive-or-negative-numbers/main.py
@@ -0,0 +1,7 @@
+a=int(input('enter the no to check whether it is +ve or -ve:'))
+if a>0:
+    print('it is positive')
+elif a<0:
+    print('it is negative')
+else:
+    print('the no 0 is neither positive or negative')
