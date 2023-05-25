e72449d11bd1228d6dda6689e3804474d1b2eecc
ADJDK <adrikdan@gmail.com>
2023-05-25 13:13:57 +0000

    Archive: Complete largest-of-the-three-number-using-nested-if

---
+++ largest-of-the-three-number-using-nested-if/main.py
@@ -0,0 +1,13 @@
+a=int(input('enter the first no:'))
+b=int(input('enter the second no:'))
+c=int(input('enter the third no:'))
+if a>b:
+    if a>c:
+        print(a,"is the largest no")
+    else:
+         print(c,"is the largest no")
+else:
+    if b>c:
+         print(b,"is the largest no")
+    else:
+         print(c,"is the largest no")
