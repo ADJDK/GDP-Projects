a6dc4efc21d577e7e88174570860f2de6d8e90e7
ADJDK <adrikdan@gmail.com>
2023-06-15 12:51:26 +0000

    Archive: Complete math-module-maximum-and-minimum-no-finding

---
+++ math-module-maximum-and-minimum-no-finding/main.py
@@ -0,0 +1,8 @@
+import math
+print('this is a program')
+num=int(input('enter the first no'))
+num1=int(input('enter the second no'))
+maxi=max(num,num1)
+print('the maximum no is {}'.format(maxi))
+mini=min(num,num1)
+print('the minimum no is {}'.format(mini))
