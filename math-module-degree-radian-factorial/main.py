e46fe6b315608e853b13f9be1457d7291f9ac38e
ADJDK <adrikdan@gmail.com>
2023-06-16 04:50:58 +0000

    Archive: Complete math-module-degree-radian-factorial

---
+++ math-module-degree-radian-factorial/main.py
@@ -0,0 +1,20 @@
+import math
+r=int(input('enter the radius:'))
+p=math.pi
+area=4*p*r**2
+print('the surface area is:',area)
+area=math.ceil(area)
+print('the area is:',area)
+num=4
+fac=math.factorial(num)
+print('the factorial is',fac)
+x=max(5,10,75)
+y=min(2,10,6)
+print(x)
+print(y)
+power=math.pow(4,3)
+print(power)
+degree=math.degrees(1)
+print('the radian 1 to degree',degree)
+radian=math.radians(1)
+print('the degree 1 to radian',radian)
