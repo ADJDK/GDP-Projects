b7d8a0f6d29a73dfc8c12b94f3415dc1b704f349
ADJDK <adrikdan@gmail.com>
2023-06-23 05:05:09 +0000

    Archive: Complete for-loop-iteration

---
+++ for-loop-iteration/main.py
@@ -0,0 +1,13 @@
+x='champion'
+for i in x:
+    print(i,end='')
+print('')
+for i in range(0,len(x)):
+    print(x[i],end='')
+print('')
+for i in range(-1,-len(x)-1,-1):
+    print(x[i],end='')
+print('')
+a=input('enter your name: ')
+for i in range(-1,-len(a)-1,-1):
+    print(a[i],end='')
