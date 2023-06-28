7c8fba5f76bb888f49e0e2ae16f48e43a632982d
ADJDK <adrikdan@gmail.com>
2023-06-28 06:15:25 +0000

    Archive: Complete nested-lists-how-to-use

---
+++ nested-lists-how-to-use/main.py
@@ -0,0 +1,10 @@
+nestedlist=[[1,2,3],[4,5,6],[7,8,9],[1,3,4]]
+print(nestedlist)
+'''print(nestedlist[0][0])'''
+
+n=[[1,2],[3,4,5,6],[7,8,9]]
+for sublist in n:
+    for x in sublist:
+        print(x)                                    #used to access each and every element of a nested list
+        
+
