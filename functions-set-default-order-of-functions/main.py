137c406d3dc199097b1f298d09e991abf307ab7e
ADJDK <adrikdan@gmail.com>
2023-07-06 06:20:29 +0000

    Archive: Complete functions-set-default-order-of-functions

---
+++ functions-set-default-order-of-functions/main.py
@@ -0,0 +1,14 @@
+def child(st4,st3,st1,st2):
+    print('the yougest child is',st4)
+
+child(st1='naymar',st2='james',st4='karl',st3='boss')
+
+print('')
+
+def country(c='India'):
+    print('i am from',c)
+
+country('usa')
+country('thailand')
+country()
+
