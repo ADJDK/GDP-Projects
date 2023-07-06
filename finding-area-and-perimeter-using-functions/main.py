2d372a1d74417e8fb878211d0306ad90f13b4bf3
ADJDK <adrikdan@gmail.com>
2023-07-06 05:51:15 +0000

    Archive: Complete finding-area-and-perimeter-using-functions

---
+++ finding-area-and-perimeter-using-functions/main.py
@@ -0,0 +1,14 @@
+def area(n1,n2):
+    print('the area is')
+    return n1*n1
+
+
+def perimeter(n1,n2):
+    print('the perimeter is')
+    return 2*(n1+n2)
+
+
+l=5
+b=10
+print(area(l,b))
+print(perimeter(l,b))
