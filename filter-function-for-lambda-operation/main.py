bb294bf5368e45ae83ad42df5f379bfb29de13c9
ADJDK <adrikdan@gmail.com>
2023-07-07 06:08:15 +0000

    Archive: Complete filter-function-for-lambda-operation

---
+++ filter-function-for-lambda-operation/main.py
@@ -0,0 +1,9 @@
+number_list=[1,4,10,16,2,6,7,19,21]
+
+filtered_list=list(filter(lambda num:num>10,number_list))
+
+print(filtered_list)                    
+
+
+#so basicaly filter is like a (if statement).....and lambda is like (for I) in...and lambda stores it to the variable
+#so we print the variable and filter we also have to put from where we are taking the (I)
