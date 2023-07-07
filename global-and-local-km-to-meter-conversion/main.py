ec7d0089b2ac472db02d8f658c8c00ac108979dd
ADJDK <adrikdan@gmail.com>
2023-07-07 06:25:15 +0000

    Archive: Complete global-and-local-km-to-meter-conversion

---
+++ global-and-local-km-to-meter-conversion/main.py
@@ -0,0 +1,13 @@
+vehicle='bike'
+
+def kmtomsec(speed):
+    vehicle='car'
+    global newspeed
+    newspeed=speed*5/18
+    print('the',vehicle,'speed is now',newspeed)
+
+speed=90
+
+kmtomsec(speed)
+
+print('the',vehicle,'speed is now',newspeed)
