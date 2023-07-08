5d8a0000a605deef622df17df97d21a87b5a74ee
ADJDK <adrikdan@gmail.com>
2023-07-08 05:20:26 +0000

    Archive: Complete modules-like-dir-import-etc-how-it-works

---
+++ modules-like-dir-import-etc-how-it-works/main.py
@@ -0,0 +1,27 @@
+import datetime
+a=datetime.datetime.now()
+print(a)
+b=dir(datetime)
+print(b)
+
+import math
+
+x=math.sqrt(81)
+y=dir(math)
+
+print(x)
+print(y)
+
+import datetime as d
+
+e=d.date.today()
+print(e)
+
+from datetime import date
+
+q=date.today()
+print(q)
+
+from datetime import *
+r=date.today()
+print(r)
